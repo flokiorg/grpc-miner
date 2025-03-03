7eb3654e470a47e46509b1ac97171ed082b1dfca
mechion <4387895+mechion@users.noreply.github.com>
2025-03-03 09:05:14 +0000

    MIT

--- common/config.go
+++ common/config.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2024 The Flokicoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 package common
 
 import "time"
